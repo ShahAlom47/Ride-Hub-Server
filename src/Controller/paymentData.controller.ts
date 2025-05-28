16543cd3776b12696befdec900c8161ffab11be5
ShahAlom47 <sahalom4729@gmail.com>
2025-05-28 10:55:31 +0600

    update  ssl init api

--- src/Controller/paymentData.controller.ts
+++ src/Controller/paymentData.controller.ts
@@ -11,32 +11,45 @@ const paymentCollection = getPaymentCollection()
 // SSLCommerz  
 const store_id = process.env.SSL_COMMERZ_STORE_ID || 'YOUR_STORE_ID';
 const store_passwd = process.env.SSL_COMMERZ_STORE_PASSWORD || 'YOUR_STORE_PASSWORD';
-const is_live = process.env.SSL_COMMERZ_IS_LIVE; // sandbox
+// const is_live = process.env.SSL_COMMERZ_IS_LIVE; // sandbox
+const is_live = false; // sandbox
 
 const sslInit = async (req: Request, res: Response): Promise<void>  => {
-    const {amount}= req.body;
+    const {email,amount}= req.body;
     const data = {
-        total_amount: amount,
+        total_amount: 120,
         currency: 'BDT',
         tran_id: 'TXN_' + Date.now(),
         success_url: 'http://localhost:3030/success',
         fail_url: 'http://localhost:3030/fail',
         cancel_url: 'http://localhost:3030/cancel',
         ipn_url: 'http://localhost:3030/ipn',
-        product_name: 'Test Product',
-        product_category: 'Test Category',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
         product_profile: 'general',
-        cus_name: 'John Doe',
-        cus_email: 'john@example.com',
+        cus_name: 'Customer Name',
+        cus_email: 'customer@example.com',
         cus_add1: 'Dhaka',
-        cus_phone: '01711111111',
+        cus_add2: 'Dhaka',
         cus_city: 'Dhaka',
-        cus_postcode: '1207',
-        cus_country: 'Bangladesh'
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
     };
 
     const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
     sslcz.init(data).then((response: any) => {
+        console.log( 'response'  ,response ,);
         res.redirect(response.GatewayPageURL);
     }).catch((err: any) => {
         res.status(500).send('Payment initiation failed.');
